73506c6117ab02117f885630dad0530c44262bd0
yash2k26 <yashbharadwaj90@gmail.com>
2026-05-09 17:13:25 +0530

    loop commits over date range

--- index.js
+++ index.js
@@ -3,12 +3,27 @@ import moment from "moment";
 import simpleGit from "simple-git";
 
 const path = "./data.json";
-const date = moment().format();
+const git = simpleGit();
 
-const data = {
-  date: date,
+const days = ["2026-05-04", "2026-05-05", "2026-05-06", "2026-05-07", "2026-05-08"];
+const rand = (lo, hi) => lo + Math.floor(Math.random() * (hi - lo + 1));
+
+const run = async () => {
+  for (const day of days) {
+    const n = rand(8, 18);
+    console.log(`>> ${day} -> ${n} commits`);
+
+    const secs = Array.from({ length: n }, () => rand(21600, 75600)).sort((a, b) => a - b);
+
+    for (const s of secs) {
+      const date = moment(`${day}T00:00:00+05:30`).add(s, "seconds").format();
+      await jsonfile.writeFile(path, { date });
+      process.env.GIT_AUTHOR_DATE = date;
+      process.env.GIT_COMMITTER_DATE = date;
+      await git.add([path]).commit(date, { "--date": date });
+    }
+  }
+  await git.push();
 };
 
-jsonfile.writeFile(path, data);
-
-simpleGit().add([path]).commit(date,{'--date':date}).push()
+run();
